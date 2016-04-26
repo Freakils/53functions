7b7b33a87292e32c624298f8ce3b419d90836553
Freakils <luvo@projectcodex.co>
2016-04-26 11:09:27 +0200

    functions

--- hello_joe.js
+++ hello_joe.js
@@ -1,3 +1,10 @@
-module.exports = function(){
-    
+module.exports = function(name) {
+  if (name === "Joe") {
+    return "Hello!";
+  }
+  if (name === "Bob") {
+    return "Hello!"
+  }
+  return "Hello, " + name.toUpperCase() + "!";
+
 };
